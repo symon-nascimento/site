fc7d4fc43d44ba158bce810269ac78bd4f30a0ee
Symon Nascimento <symon_nascimento@hotmail.com>
2022-09-28 17:46:20 -0300

    wip: conteudo de card skills

--- components/CardSkills.tsx
+++ components/CardSkills.tsx
@@ -13,10 +13,10 @@ const dataSkills = dataCardSkills
 
 const CardSkiils = () => {
     return (
-        <Grid spacing={2}>
+        <Grid item xs={12} spacing={2}>
             {
                 dataSkills.map((cards, index) => (
-                    <Grid key={index} sx={{ display: "inline-block", padding: '4px', width: '300px'}}>
+                    <Grid key={index} item xs={3} sx={{ display: "inline-block", padding: '4px', width: '300px'}}>
                         <CreateSkills imageName={cards.imageName} title={cards.title} text={cards.text} />
                     </Grid>
                 ))
--- data/cardSkills.ts
+++ data/cardSkills.ts
@@ -12,4 +12,44 @@ export const dataCardSkills: CardSkillsProps[] = [
         title: 'Docker',
         text: 'Trabalhando com Docker',
     },
+    {
+        imageName: 'docker-compose.jpg',
+        title: 'Docker Compose',
+        text: 'Trabalhando Docker Compose',
+    },
+    {
+        imageName: 'waypoint.png',
+        title: 'Hashicorp Waypoint',
+        text: 'Trabalhando com Waypoint',
+    },
+    {
+        imageName: 'gitlab.png',
+        title: 'Gitlab',
+        text: 'Trabalhando com Gitlab',
+    },
+    {
+        imageName: 'aws.png',
+        title: 'Amazon Web Services',
+        text: 'Trabalhando com AWS',
+    },
+    {
+        imageName: 'nx.png',
+        title: 'NX Workrspace',
+        text: 'Trabalhando com NX Monorepo',
+    },
+    {
+        imageName: 'golang.png',
+        title: 'Golang',
+        text: 'Trabalhando Golang',
+    },
+    {
+        imageName: 'javascript.png',
+        title: 'Javascript e Typescript',
+        text: 'Trabalhando Javascript e Typescript',
+    },
+    {
+        imageName: 'nextjs.png',
+        title: 'Next JS',
+        text: 'Trabalhando Next JS',
+    },
 ]
